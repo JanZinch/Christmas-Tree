3c3ab6f1e0bdcb1f4bed505482460f24c0f5caa5
JanZinch <jan.zinchanka@mail.ru>
2021-10-17 16:30:04 +0300

    acceleration

--- Christmas-Tree/Assets/Scripts/RotatingPlatform.cs
+++ Christmas-Tree/Assets/Scripts/RotatingPlatform.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 
 public class RotatingPlatform : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotationSpeed = new Vector3(0.0f, 2.5f, 0.0f);
+    [SerializeField] private float _maxSpeedIncrement = 1.5f;
+    [SerializeField] private float _minSpeedIncrement = 0.5f;
+
 
     //[SerializeField] private List<Transform> _attachedObjects = null;
 
@@ -15,6 +18,7 @@ public class RotatingPlatform : MonoBehaviour
 
     private void Awake()
     {
+        Debug.Log("First stage. Spped: " + _rotationSpeed);
         //_attachedObjects = new List<Transform>();
     }
 
@@ -31,7 +35,7 @@ public class RotatingPlatform : MonoBehaviour
         if (_stage == 0 && score >= 100.0f || _stage == 1 && score >= 200.0f || _stage == 2 && score >= 300.0f)
         {
 
-            Debug.Log("NEXT STAGE");
+            
             NextStage();
         }
         else if (_stage == 3) { 
@@ -60,10 +64,60 @@ public class RotatingPlatform : MonoBehaviour
             direction = 1;
         }
 
-        _rotationSpeed = new Vector3(0.0f, UnityEngine.Random.Range(direction * 2.5f, direction * 5.0f), 0.0f);
+        
+        Vector3 _targetSpeed = new Vector3(0.0f, UnityEngine.Random.Range(Mathf.Abs(_rotationSpeed.y) + _minSpeedIncrement, Mathf.Abs(_rotationSpeed.y) + _maxSpeedIncrement), 0.0f);
+        _targetSpeed *= direction;
+
+        Debug.Log("Next stage. Spped: " + _targetSpeed);
+
+        StartCoroutine(AccelerateRotation(_targetSpeed));
 
         _stage++;
     }
+
+
+    private bool SafeEquals(float a, float b) {
+
+        const float e = 0.1f;
+
+        Debug.Log(Mathf.Abs(a - b));
+
+        return Mathf.Abs(a - b) < e;
+    
+    }
+
+    private IEnumerator AccelerateRotation(Vector3 _targetSpeed) {
+
+        float Acceleration = 0.1f;
+
+        if (_rotationSpeed.y > _targetSpeed.y) {
+
+            Acceleration *= -1;
+        }
+                
+        WaitForSeconds wait = new WaitForSeconds(0.1f);
+
+        Debug.Log("Target: " + _targetSpeed);
+
+
+        while (!SafeEquals(_rotationSpeed.y, _targetSpeed.y)) {
+
+            Debug.Log("Current: " + _rotationSpeed);
+
+            _rotationSpeed.y += Acceleration;
+
+            yield return wait;
+        
+        }
+
+
+        Debug.Log("Complete!");
+
+        yield return null;
+    
+    }
+
+
 
     private void FinishSatge() { 
     
