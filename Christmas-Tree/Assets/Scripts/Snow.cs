0a6cffbd19f4d65b613b0ea546b993cdea3e948a
JanZinch <jan.zinchanka@mail.ru>
2021-11-14 00:14:44 +0300

    batching

---
+++ Christmas-Tree/Assets/Scripts/Snow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particles = null;
+    [SerializeField] private float _startSimulationTime = 10.0f;
+
+    void Start()
+    {
+        _particles.Simulate(_startSimulationTime);
+        _particles.Play();
+    }
+
+    void Update()
+    {
+    }
+}
